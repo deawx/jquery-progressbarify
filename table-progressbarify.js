9220ebd701b439450018e5cd75199ed7e79086af
Rob Farr <robfarr@users.noreply.github.com>
2016-09-17 10:51:35 +0100

    Initial commit.
    Progressbarify plugin & examples in html file.

---
+++ table-progressbarify.js
@@ -0,0 +1,81 @@
+(function($){
+
+    $.fn.progressbarify = function(args) {
+
+        var table = $(this);
+        var tbody = (table.find('tbody').length > 0) ? table.find('tbody') : table;
+
+        args = parseArgs(args);
+
+        var values           = getColumnValues(tbody, args.targetColumn);
+        var maxValue         = getMaximumColumnValue(values);
+        var percentageValues = getPercentageValues(values, maxValue);
+        var gradients        = getPercentageBackgroundGradientValues(percentageValues, args.primaryColour, args.secondaryColour);
+
+        applyGradientBackgrounds(tbody, args.targetColumn, gradients);
+
+        function parseArgs(args) {
+
+            if(typeof args == "undefined") args = [];
+
+            if(typeof args.primaryColour == "undefined") {
+                args.primaryColour = '#31b0d5';
+            }
+
+            if(typeof args.secondaryColour == "undefined") {
+                args.secondaryColour = 'transparent';
+            }
+
+            if(typeof args.targetColumn !== "undefined" && args.targetColumn.isNumeric() && args.targetColumn >= 0) {
+                args.targetColumn = args.targetColumn;
+            }else if(typeof table.attr('data-progressbarify') !== "undefined" && table.attr('data-progressbarify') >= 0) {
+                args.targetColumn = table.attr('data-progressbarify');
+            }else{
+                args.targetColumn = 0;
+            }
+
+            return args;
+
+        }
+
+        function getColumnValues(table, targetColumn) {
+            var values = [];
+            $(table).find('tr').each(function() {
+                var value = $(this).children().eq(targetColumn).text();
+                values.push(value);
+            });
+            return values;
+        }
+
+        function getMaximumColumnValue(values) {
+            var max = 0;
+            for(var i=0; i<values.length; i++) {
+                if(values[i] > max) max = values[i];
+            }
+            return max;
+        }
+
+        function getPercentageValues(values, maxValue) {
+            return values.map(function(value) {
+                return (value / maxValue) * 100;
+            });
+        }
+
+        function getPercentageBackgroundGradientValues(percentageValues, primaryColour, secondaryColour) {
+            return percentageValues.map(function(percentage) {
+                percentage = Math.round(percentage);
+                return 'linear-gradient(90deg, ' + primaryColour + ' ' + percentage + '%, ' + primaryColour + ' ' + percentage + '%, ' + secondaryColour + ' ' + percentage + '%)';
+            });
+        }
+
+        function applyGradientBackgrounds(table, targetColumn, gradients) {
+            table.find('tr').each(function(i) {
+                $(this).children().eq(targetColumn).css('background-image', gradients[i]);
+            });
+        }
+
+        return this;
+
+    }
+
+})(jQuery);
